f5759be939dc6a6e56899b8fb1d0fe9b36f11b70
PriyadharsiniRamesh <priyadharsinir98@gmail.com>
2017-08-15 11:25:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,4 +9,24 @@ element.innerHTML = 'New value';
 var img=document.getElementById("madi");
 img.onclick= function(){
     img.style.marginLeft='100px';
-}
+};
+
+
+//submit name
+var nameInput = document.getElementById('name');
+var name =nameInput.value;
+var submit= document.getElemntById('submit-btn');
+submit.onclick = function() {
+    //make a request to the server and send the name
+    
+    //capture the list of names and render it as a list
+    var names= ['name1','name2','name3'];
+    var list = '';
+    for (var i=0;i<names.length;i++)
+    {
+        list == '<li>' + names[i] + '</li>';
+        
+    }
+    var ul = document.getElementById('name-list');
+    ul.innerHTML = list;
+};
